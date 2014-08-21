dab34f07b852fab4ba9ac5b6436c4f6416d1246b
Rbn3D <ruben3d3@gmail.com>
2014-08-21 12:32:05 +0200

    Basic Disqus stuff working

---
+++ config.dist.php
@@ -0,0 +1,18 @@
+<?php
+// Disqus Audience Sync config variables (rename to 'config.php' and set the propper values)
+
+define("DisqusApiPublic", "...");
+
+define("DisqusApiSecret", "...");
+
+define("DisqusShortname", "...");
+
+define("BaseSitePath", "...");
+
+define("DisqusAudSyncAccessTokenURL", "...");
+
+// --- Debug
+
+define("DisqusAudSyncAccessToken", "..."); // User access token should be different for each user, but just for debug
+
+?>
